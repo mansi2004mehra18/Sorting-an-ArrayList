98a709817cb7a892484aae04627007a22f6b0ce4
Mansi Mehra <mansi2004mehra18@gmail.com>
2025-03-11 14:44:45 +0530

    modificatons

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Sorting-an-ArrayList
 
 sorting an arraylist using build in function/class called collections.
+
+Comparator - defines the function logic of arrangment.
--- Sorting.java
+++ Sorting.java
@@ -13,7 +13,11 @@ public class Sorting {
         list.add(6);
 
         System.out.println(list);
-        Collections.sort(list);
+        Collections.sort(list); //ascending
+        System.out.println(list);
+
+        //descending
+        Collections.sort(list, Collections.reverseOrder());
         System.out.println(list);
     }
 }
